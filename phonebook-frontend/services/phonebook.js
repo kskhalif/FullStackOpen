2fb13321f979848545f2f775f92af04d63d24c89
Kacem Khalife <91025619+kskhalif@users.noreply.github.com>
2022-07-06 19:48:14 -0400

    Add files via upload

---
+++ phonebook-frontend/services/phonebook.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const baseUrl = '/api/phonebook';
+
+const get = (filter = '') => {
+    const request = axios.get(`${baseUrl}/${filter}`);
+    return request.then(response => response.data);
+};
+
+const add = (newContact) => {
+    const request = axios.post(baseUrl, newContact);
+    return request.then(response => response.status);
+};
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`);
+    return request.then(response => response.status);
+};
+
+const update = (id, updatedContact) => {
+    const request = axios.put(`${baseUrl}/${id}`, updatedContact);
+    return request.then(response => response.status);
+};
+
+export default { get, add, remove, update };
