6421e6f18e35bba1106b05b2b5fe066217399bd6
Kacem Khalife <91025619+kskhalif@users.noreply.github.com>
2022-07-26 21:52:09 -0400

    Add files via upload

--- blogList-frontend/components/Blogs.js
+++ blogList-frontend/components/Blogs.js
@@ -1,51 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import blogService from '../services/blogList';
 
 const Blog = (props) => {
-  if (props.canRemove) {
-    return (
-      <li>
-        <a href={props.blog.url} target='_blank'>
-          {props.blog.title}
-        </a>
-        {' '}
-        by {props.blog.author}, likes: {props.blog.likes}
-        {' '}
-        <button onClick={props.like}>like</button>
-        <button onClick={props.remove}>remove</button>
-      </li>
-    );
-  }
-  else if (props.canLike) {
-    return (
-      <li>
-        <a href={props.blog.url} target='_blank'>
-          {props.blog.title}
-        </a>
-        {' '}
-        by {props.blog.author}, likes: {props.blog.likes}
-        {' '}
-        <button onClick={props.like}>like</button>
-      </li>
-    );
-  }
-  else {
-    return (
-      <li>
-        <a href={props.blog.url} target='_blank'>
-          {props.blog.title}
-        </a>
-        {' '}
-        by {props.blog.author}, likes: {props.blog.likes}
-      </li>
-    );
-  }
-};
+  const [label, setLabel] = useState(props.initialLabel);
 
-const Blogs = (props) => {
-  const likeBlog = async (blog) => {
+  const likeBlog = async () => {
     try {
-      await blogService.like(blog._id.toString());
+      await blogService.like(props.blog._id.toString());
+      if (label === 'like') setLabel('unlike');
+      else setLabel('like');
       props.refetch();
     }
     catch (exception) {
@@ -54,6 +17,28 @@ const Blogs = (props) => {
     }
   };
 
+  const likeButton = props.canLike 
+    ? <button onClick={likeBlog}>{label}</button>
+    : null;
+    
+  const removeButton = props.canRemove
+    ? <button onClick={props.removeBlog}>remove</button>
+    : null;
+  
+  return (
+    <li>
+      <a href={props.blog.url} target='_blank'>
+        {props.blog.title}
+      </a>
+      {' '}
+      by {props.blog.author}, likes: {props.blog.likes.length}
+      {' '}
+      {likeButton} {removeButton}
+    </li>
+  );
+};
+
+const Blogs = (props) => {
   const removeBlog = async (blog) => {
     if (confirm(`Remove "${blog.title}"?`)) {
       try {
@@ -69,26 +54,36 @@ const Blogs = (props) => {
     }
   };
 
-  return (
-    <ul>
-      {props.blogs.map(blog => {
-        const canLike = props.user !== null;
-        const canRemove = props.user !== null
-          ? props.user.username === blog.user.username
-          : false;
-        return (
-          <Blog
-            key={blog._id.toString()}
-            blog={blog}
-            canLike={canLike}
-            canRemove={canRemove}
-            like={() => likeBlog(blog)}
-            remove={() => removeBlog(blog)}
-          />
-        );
-      })}
-    </ul>
-  );
+  if (props.user !== null) {
+    return (
+      <ul>
+        {props.blogs.map(blog => {
+          const canLike = props.user !== null;
+          const canRemove = props.user !== null
+            ? props.user.username === blog.user.username
+            : false;
+  
+          const initialLabel = blog.likes.find(user => 
+            user.username === props.user.username)
+            ? 'unlike'
+            : 'like';
+  
+          return (
+            <Blog
+              key={blog._id.toString()}
+              blog={blog}
+              canLike={canLike}
+              canRemove={canRemove}
+              initialLabel={initialLabel}
+              removeBlog={() => removeBlog(blog)}
+              refetch={props.refetch}
+              setMessage={props.setMessage}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
 };
 
 export default Blogs;
