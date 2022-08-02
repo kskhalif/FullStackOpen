3d1f70b1e7d1745ff88a85d2c7ea8c73e297f3a3
Kacem Khalife <91025619+kskhalif@users.noreply.github.com>
2022-08-01 22:14:47 -0400

    Add files via upload

--- notes-redux/index.js
+++ notes-redux/index.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './App.css';
 import { legacy_createStore as createStore } from 'redux';
-
 import noteReducer from './reducers/noteReducer';
 
 const store = createStore(noteReducer);
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-});
+const generateID = () =>  Number((Math.random() * 1000000).toFixed(0));
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-});
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateID()
+    }
+  };
+};
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
+  };
+};
 
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = '';
+    store.dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id));
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
       <ul>
         {store.getState().map(note => 
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : null}</strong>
           </li>
         )}
